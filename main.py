a3cbc494229810c088d44afeb87552bdfb9df643
Daniel <burvaldaniel@gmail.com>
2026-04-14 18:23:26 +0200

    init

--- main.py
+++ main.py
@@ -2,20 +2,14 @@ from deepagents import create_deep_agent
 
 
 
-@tool
-def get_weather(city: str) -> str:
-    """Get weather for a given city."""
-    return f"It's always sunny in {city}!"
-
 agent = create_deep_agent(
     model="qwen3:9b",
-    tools=[get_weather],
     system_prompt="You are a helpful assistant",
 )
 
 result = agent.invoke({
     "messages": [
-        {"role": "user", "content": "what is the weather in Prague"}
+        {"role": "user", "content": "How are you?"}
     ]
 })
 
